08bb7418a1e31a3b30c36671189abb0faefe6b92
Trịnh Tuyền <cobatuoc@gmail..com>
2025-10-24 17:12:15 +0700

    24/10

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,6 +109,25 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
+
+  //
+  //
+   async getProductById(req, res, next) {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      return res.status(200).json(product);}
+    catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    } }
 }
+
 
 module.exports = ProductController;
